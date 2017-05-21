1c5682ca2cbdfa1d9df9966397429c5bfe76264d
danvolchek <cat@catalicio.us>
2017-05-20 20:18:27 -0500

    Added auto stacker mod

---
+++ AutoStacker/AutoStacker/ModEntry.cs
@@ -0,0 +1,101 @@
+﻿using StardewModdingAPI;
+using StardewValley;
+using System;
+using System.Collections.Generic;
+using StardewModdingAPI.Events;
+
+namespace AutoStacker
+{
+    class ModEntry : Mod
+    {
+        private ModConfig config;
+
+        public override void Entry(IModHelper helper)
+        {
+            config = helper.ReadConfig<ModConfig>();
+            ControlEvents.KeyPressed += this.KeyPressed;
+        }
+
+        private void KeyPressed(object sender, EventArgsKeyPressed e)
+        {
+            String key = e.KeyPressed.ToString().ToLower();
+
+            if (key == config.ActivationKey.ToLower())
+            {
+                stackOwnInventory();
+
+            }
+        }
+
+        private void stackOwnInventory()
+        {
+            List<Item> items = Game1.player.items.FindAll(it => it != null);
+            items.Reverse();
+            foreach (Item item in items)
+            {
+                if (item.Stack == item.maximumStackSize() || item.Stack == 0)
+                    continue;
+
+                foreach (Item stackOnMe in Game1.player.items.FindAll(it => it != null && it != item && it.canStackWith(item) && it.getRemainingStackSpace() > 0))
+                {
+                    int remain = stackOnMe.getRemainingStackSpace();
+                    int add = Math.Min(remain, item.Stack);
+
+                    stackOnMe.addToStack(add);
+                    item.Stack -= add;
+
+                    if (item.Stack == 0)
+                        break;
+
+                }
+            }
+
+            for (int i = 0; i < Game1.player.items.Count; i++)
+            {
+                Item it = Game1.player.items[i];
+                if (it != null && it.Stack == 0)
+                {
+                    swapStack(findFirstNonEmptyStack(it, i + 1), it);
+
+                }
+            }
+
+            for (int i = 0; i < Game1.player.items.Count; i++)
+            {
+                Item it = Game1.player.items[i];
+                if (it != null && it.Stack == 0)
+                {
+                    Game1.player.items[i] = null;
+
+                }
+            }
+
+
+            
+        }
+
+        private Item findFirstNonEmptyStack(Item a, int from)
+        {
+            for (int i = from; i < Game1.player.items.Count; i++)
+            {
+                Item it = Game1.player.items[i];
+                if (it != null && it.Stack != 0 && it.canStackWith(a))
+                {
+                    return it;
+                }
+            }
+            return null;
+        }
+
+        private void swapStack(Item a, Item b)
+        {
+            if (a == null || b == null)
+                return;
+            if (!a.canStackWith(b))
+                return;
+            int temp = a.Stack;
+            a.Stack = b.Stack;
+            b.Stack = temp;
+        }
+    }
+}
---
+++ AutoStacker/README.md
@@ -0,0 +1,4 @@
+# Auto Stacker
+
+
+See [This link](http://www.nexusmods.com/stardewvalley/mods/1184?) for the NexusMods mod page, which has a description, screenshots, and a download of the built mod.
