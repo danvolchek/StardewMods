2954565a3fdc782b8d4b3da7e526f22ce5a76793
volchek2 <volchek2@illinois.edu>
2017-12-03 01:41:42 -0600

    add no crows

---
+++ NoCrows/NoCrows/Injector.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace NoCrows
+{
+    //Method replacement code is from here:
+    //https://stackoverflow.com/questions/7299097/dynamically-replace-the-contents-of-a-c-sharp-method
+    class Injector
+    {
+        public static void Replace(Type toReplace, string toReplaceMethod, Type replaceWith, string replaceWithMethod)
+        {
+            MethodInfo methodToReplace = toReplace.GetMethod(toReplaceMethod, BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public);
+            MethodInfo methodToInject = replaceWith.GetMethod(replaceWithMethod, BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public);
+            RuntimeHelpers.PrepareMethod(methodToReplace.MethodHandle);
+            RuntimeHelpers.PrepareMethod(methodToInject.MethodHandle);
+
+            unsafe
+            {
+                if (IntPtr.Size == 4)
+                {
+                    int* inj = (int*)methodToInject.MethodHandle.Value.ToPointer() + 2;
+                    int* tar = (int*)methodToReplace.MethodHandle.Value.ToPointer() + 2;
+#if DEBUG
+                    byte* injInst = (byte*)*inj;
+                    byte* tarInst = (byte*)*tar;
+
+                    int* injSrc = (int*)(injInst + 1);
+                    int* tarSrc = (int*)(tarInst + 1);
+
+                    *tarSrc = (((int)injInst + 5) + *injSrc) - ((int)tarInst + 5);
+#else
+                    *tar = *inj;
+#endif
+                }
+                else
+                {
+
+                    long* inj = (long*)methodToInject.MethodHandle.Value.ToPointer() + 1;
+                    long* tar = (long*)methodToReplace.MethodHandle.Value.ToPointer() + 1;
+#if DEBUG
+                    byte* injInst = (byte*)*inj;
+                    byte* tarInst = (byte*)*tar;
+
+
+                    int* injSrc = (int*)(injInst + 1);
+                    int* tarSrc = (int*)(tarInst + 1);
+
+                    *tarSrc = (((int)injInst + 5) + *injSrc) - ((int)tarInst + 5);
+#else
+                    *tar = *inj;
+#endif
+                }
+            }
+        }
+    }
+}
