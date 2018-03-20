2bd061f9bc6a3f19727d769f259eca6b0a0eba38
danvolchek <volchek2@illinois.edu>
2018-03-19 21:45:34 -0500

    fix animal eating grass bug, again

--- BetterHay/BetterHay/ModEntry.cs
+++ BetterHay/BetterHay/ModEntry.cs
@@ -21,7 +21,7 @@ namespace BetterHay
         public GameLocation currentLocation = null;
 
         //Last list of terrain features
-        private Dictionary<Vector2, TerrainFeature> lastTerrainFeatures;
+        private Dictionary<Vector2, TerrainFeature> lastTerrainFeatures = null;
 
         private Random dropGrassStarterRandom;
 
@@ -33,7 +33,7 @@ namespace BetterHay
 
             if (config.EnableGettingHayFromGrassAnytime)
             {
-                GameEvents.UpdateTick += this.UpdateTick;
+                GameEvents.EighthUpdateTick += this.EighthUpdateTick;
                 LocationEvents.CurrentLocationChanged += this.CurrentLocationChanged;
             }
 
@@ -45,28 +45,59 @@ namespace BetterHay
         }
 
         //Update tick - check for removed grass and spawn hay if appropriate
-        private void UpdateTick(object sender, EventArgs e)
+        private void EighthUpdateTick(object sender, EventArgs e)
         {
             if (Game1.currentLocation?.terrainFeatures == null || lastTerrainFeatures == null || Game1.currentLocation != currentLocation)
                 return;
 
             foreach (KeyValuePair<Vector2, TerrainFeature> item in lastTerrainFeatures)
-                if (!Game1.currentLocation.terrainFeatures.Contains(item))
+                if (!Game1.currentLocation.terrainFeatures.Contains(item) && item.Value is Grass)
                     if (((Game1.IsMultiplayer ? Game1.recentMultiplayerRandom : new Random((int)((double)Game1.uniqueIDForThisGame + (double)item.Key.X * 1000.0 + (double)item.Key.Y * 11.0))).NextDouble() < 0.5))
                         if (Game1.player.CurrentTool is MeleeWeapon && (Game1.player.CurrentTool.Name.Contains("Scythe") || Game1.player.CurrentTool.parentSheetIndex == 47))
                         {
-                            if (dropGrassStarterRandom.NextDouble() < config.ChanceToDropGrassStarterInsteadOfHay)
+                            if (IsWithinRange(Game1.player.getTileLocation(), item.Key, 3))
                             {
-                                if (!BetterHayGrass.TryAddItemToInventory(297) && config.DropHayOnGroundIfNoRoomInInventory)
-                                    BetterHayGrass.DropOnGround(item.Key, 297);
+                                if (dropGrassStarterRandom.NextDouble() < config.ChanceToDropGrassStarterInsteadOfHay)
+                                {
+                                    AttemptToGiveGrassStarter(item.Key, Game1.getFarm().piecesOfHay == Utility.numSilos() * 240);
+                                }
+                                else if (Game1.getFarm().tryToAddHay(1) != 0)
+                                    if (!BetterHayGrass.TryAddItemToInventory(178) && config.DropHayOnGroundIfNoRoomInInventory)
+                                        BetterHayGrass.DropOnGround(item.Key, 178);
                             }
-                            else if ((Game1.getLocationFromName("Farm") as Farm).tryToAddHay(1) != 0)
-                                if (!BetterHayGrass.TryAddItemToInventory(178) && config.DropHayOnGroundIfNoRoomInInventory)
-                                    BetterHayGrass.DropOnGround(item.Key, 178);
                         }
 
             lastTerrainFeatures = Game1.currentLocation?.terrainFeatures?.ToDictionary(entry => entry.Key,
                                   entry => entry.Value);
+        }
+
+        //Correctly give grass starters instead of hay when silos are full and not full
+        private void AttemptToGiveGrassStarter(Vector2 location, bool silosAreFull)
+        {
+            if (silosAreFull)
+            {
+                if (!BetterHayGrass.TryAddItemToInventory(297) && config.DropHayOnGroundIfNoRoomInInventory)
+                    BetterHayGrass.DropOnGround(location, 297);
+            }
+            else
+            {
+                bool added = BetterHayGrass.TryAddItemToInventory(297);
+                if (!added && config.DropHayOnGroundIfNoRoomInInventory)
+                {
+                    BetterHayGrass.DropOnGround(location, 297);
+                    added = true;
+                }
+                if (added)
+                    Game1.getFarm().piecesOfHay--;
+            }
+        }
+
+        //Returns whether the first vector is with range of the second, in euclidian distance
+        private bool IsWithinRange(Vector2 first, Vector2 second, double range)
+        {
+            if (second == null || first == null)
+                return false;
+            return Math.Sqrt(Math.Pow(first.X - second.X, 2) + Math.Pow(first.Y - second.Y, 2)) < range;
         }
 
         //Update the last list of terrain features
