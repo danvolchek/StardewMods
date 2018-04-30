6b282f1875df8af5f95beddd715ea01dfdde14f2
Dan Volchek <volchek2@illinois.edu>
2018-04-30 18:00:49 -0500

    update to SDV 1.3

---
+++ NoFenceDecay/NoFenceDecay/FenceFinder.cs
@@ -0,0 +1,47 @@
+﻿using StardewModdingAPI;
+using StardewValley;
+using StardewValley.Locations;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NoFenceDecay
+{
+    /// <summary>Finds all fences in the game.</summary>
+    internal class FenceFinder
+    {
+        /// <summary>Avoid infinite recursion for mods that mess up <see cref="StardewValley.Buildings.Building.indoors"/>.</summary>
+        private IList<GameLocation> searchedLocations = new List<GameLocation>();
+
+        /// <summary>Finds all fences in the game.</summary>
+        internal IEnumerable<Fence> GetFences()
+        {
+            this.searchedLocations.Clear();
+            if (Context.IsWorldReady)
+                foreach (Fence f in GetFences(Game1.locations))
+                    yield return f;
+        }
+
+        /// <summary>Finds all fences in the given <see cref="GameLocation"/>s.</summary>
+        private IEnumerable<Fence> GetFences(IEnumerable<GameLocation> locations)
+        {
+            foreach (Fence f in locations.SelectMany(loc => GetFences(loc)))
+                yield return f;
+        }
+
+        /// <summary>Finds all fences in the given <see cref="GameLocation"/>.</summary>
+        private IEnumerable<Fence> GetFences(GameLocation l)
+        {
+            if (!this.searchedLocations.Contains(l))
+            {
+                this.searchedLocations.Add(l);
+                foreach (Fence f in l.Objects.Values.OfType<Fence>())
+                    yield return f;
+
+                if (l is BuildableGameLocation bLoc)
+                    foreach (Fence f in GetFences(bLoc.buildings.Select(item => item.indoors.Value).Where(item => item != null)))
+                        yield return f;
+            }
+
+        }
+    }
+}
---
+++ NoFenceDecay/NoFenceDecay/NoFenceDecayMod.cs
@@ -0,0 +1,26 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using System;
+
+namespace NoFenceDecay
+{
+    /// <summary>Stops fences from decaying.</summary>
+    internal class NoFenceDecayMod : Mod
+    {
+        /// <summary>Used to find fences.</summary>
+        private FenceFinder finder = new FenceFinder();
+
+        public override void Entry(IModHelper helper)
+        {
+            TimeEvents.AfterDayStarted += this.TimeEvents_AfterDayStarted;
+        }
+
+        /// <summary>When every day starts, repair all fences.</summary>
+        private void TimeEvents_AfterDayStarted(object sender, EventArgs e)
+        {
+            foreach (Fence fence in this.finder.GetFences())
+                fence.repair();
+        }
+    }
+}
