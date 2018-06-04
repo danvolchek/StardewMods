eec791f085dc897ae50c256f8407378ae65bcafa
Dan Volchek <volchek2@illinois.edu>
2018-06-03 19:40:58 -0700

    [No Crows] migrate to harmony for crossplatform support

---
+++ NoCrows/NoCrows/AddCrowsPatch.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using Harmony;
+
+namespace NoCrows
+{
+    [HarmonyPatch]
+    internal class AddCrowsPatch
+    {
+        private static MethodBase TargetMethod()
+        {
+            return NoCrowsMod.GetSDVType("Farm").GetMethod("addCrows");
+        }
+
+        private static bool Prefix()
+        {
+            return false;
+        }
+    }
+}
---
+++ NoCrows/NoCrows/NoCrowsMod.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection;
+using Harmony;
+using StardewModdingAPI;
+
+namespace NoCrows
+{
+    public class NoCrowsMod : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            HarmonyInstance harmony = HarmonyInstance.Create("cat.nocrows");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        //Big thanks to Routine for this workaround for mac users.
+        //https://github.com/Platonymous/Stardew-Valley-Mods/blob/master/PyTK/PyUtils.cs#L117
+        /// <summary>Gets the correct type of the object, handling different assembly names for mac/linux users.</summary>
+        internal static Type GetSDVType(string type)
+        {
+            const string prefix = "StardewValley.";
+            Type defaultSDV = Type.GetType(prefix + type + ", Stardew Valley");
+
+            return defaultSDV ?? Type.GetType(prefix + type + ", StardewValley");
+        }
+    }
+}
