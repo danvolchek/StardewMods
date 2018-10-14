5ac041bf4e2563cca7adccf0e8ed0f28b0d98c84
danvolchek <volchek2@illinois.edu>
2018-10-13 19:03:57 -0500

    [Better Artisan Good Icons] update item handling to not crash on CFR products

--- BetterArtisanGoodIcons/BetterArtisanGoodIcons/ArtisanGoodTextureProvider.cs
+++ BetterArtisanGoodIcons/BetterArtisanGoodIcons/ArtisanGoodTextureProvider.cs
@@ -40,46 +40,71 @@ namespace BetterArtisanGoodIcons
         }
 
         /// <summary>Gets the name of the source item used to create the given item.</summary>
-        private string GetSourceName(SObject item, int sourceIndex)
+        private bool GetSourceName(SObject item, int sourceIndex, out string sourceName)
         {
-            //If we failed to find the source of the item, and it happens to be the base item, call it Base. Otherwise return null for failure, which nothing will match.
-            if (sourceIndex == -1)
+            //If the item name is equivalent to the base good, return _Base.
+            if (item.Name == this.good.ToString())
             {
-                if (item.Name == this.good.ToString())
-                    return "_Base";
-                return null;
+                sourceName = "_Base";
+                return true;
             }
-            //Lookup the name from the game's object information.
-            return Game1.objectInformation[sourceIndex].Split('/')[0];
+
+            //Lookup the name from the game's object information, or null if not found (a custom item that has its sourceIndex set incorrectly).
+            if (Game1.objectInformation.TryGetValue(sourceIndex, out string information))
+            {
+                sourceName = information.Split('/')[0];
+                return true;
+            }
+
+            sourceName = null;
+            return false;
         }
 
         /// <summary>Gets the index of the source item used to create the given item name.</summary>
-        private int GetIndexOfSource(SObject item)
+        private bool GetIndexOfSource(SObject item, out int index)
         {
             //Use preservedParentSheetIndex for wine, jelly, pickles, and juice
             if (item.preservedParentSheetIndex.Value != 0)
-                return item.preservedParentSheetIndex.Value;
+            {
+                index = item.preservedParentSheetIndex.Value;
+                return true;
+            }
 
             //Use honeyType for honey
             //Hardcode Wild honey using the sunflower image, since it is not called "Sunflower honey"
             if (this.good == ArtisanGood.Honey && item.honeyType.Value.HasValue)
-                return item.honeyType.Value.Value != SObject.HoneyType.Wild ? (int)item.honeyType.Value.Value : 421;
+            {
+                index = item.honeyType.Value.Value != SObject.HoneyType.Wild ? (int)item.honeyType.Value.Value : 421;
+                return true;
+            }
 
-            //Return -1 on failure
-            return -1;
+            index = -1;
+            return false;
         }
 
         /// <summary>Gets the info needed to draw the right texture for the given item.</summary>
         internal bool GetDrawInfo(SObject item, ref Texture2D textureSheet, ref Rectangle mainPosition, ref Rectangle iconPosition)
         {
-            int sourceIndex = this.GetIndexOfSource(item);
-            string sourceName = this.GetSourceName(item, sourceIndex);
-            if (item.ParentSheetIndex != (int)this.good || sourceName == null || !this.positions.TryGetValue(sourceName, out mainPosition))
+            //Only yield new textures for base items. If removed, everything *should* still work, but it needs more testing.
+            if (item.ParentSheetIndex != (int)this.good)
+                return false;
+
+            //If the index of the source item can't be found, exit.
+            if (!this.GetIndexOfSource(item, out int sourceIndex))
+                return false;
+
+            //CFR likes to set the preservedParentSheetIndex to the negative of the source parentSheetIndex, for some reason.
+            if (sourceIndex < 0)
+                sourceIndex *= -1;
+
+            //Get the name of the item from its index, and from that, a new sprite.
+            if (!this.GetSourceName(item, sourceIndex, out string sourceName) || !this.positions.TryGetValue(sourceName, out mainPosition))
                 return false;
 
             textureSheet = this.spriteSheet;
-            iconPosition = sourceIndex != -1 ? Game1.getSourceRectForStandardTileSheet(Game1.objectSpriteSheet, sourceIndex, 16, 16): Rectangle.Empty;
-
+            iconPosition = sourceIndex != -1
+                ? Game1.getSourceRectForStandardTileSheet(Game1.objectSpriteSheet, sourceIndex, 16, 16)
+                : Rectangle.Empty;
             return true;
         }
     }
