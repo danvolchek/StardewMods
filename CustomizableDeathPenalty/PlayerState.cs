177fbb1b51e4d17841f23e2b429ac5af586420f6
danvolchek <volchek2@illinois.edu>
2019-03-05 02:04:59 -0600

    [Customizable Death Penalty] fix losing slingshot ammo and fishing rod bait and bobber

--- CustomizableDeathPenalty/PlayerState.cs
+++ CustomizableDeathPenalty/PlayerState.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
 using StardewValley;
+using StardewValley.Menus;
+using StardewValley.Tools;
 
 namespace CustomizableDeathPenalty
 {
@@ -19,14 +21,42 @@ namespace CustomizableDeathPenalty
             this.deepestMineLevel = d;
             this.lowestLevelReached = l;
             this.inventory = new List<Item>();
-            foreach (var item in i)
+            foreach (Item item in i)
                 if (item == null)
                 {
                     this.inventory.Add(null);
                 }
                 else
                 {
-                    var copy = item.getOne();
+                    Item copy;
+                    if (item is Slingshot slingshot)
+                    {
+                        Slingshot slingshotCopy = slingshot.getOne() as Slingshot;
+                        slingshotCopy.InitialParentTileIndex = slingshot.InitialParentTileIndex;
+                        slingshotCopy.CurrentParentTileIndex = slingshot.InitialParentTileIndex;
+                        slingshotCopy.IndexOfMenuItemView = slingshot.IndexOfMenuItemView;
+
+                        slingshotCopy.attachments[0] = slingshot.attachments[0].getOne() as StardewValley.Object;
+                        slingshotCopy.attachments[0].Stack = slingshot.attachments[0].Stack;
+                        copy = slingshotCopy;
+                    }else if (item is FishingRod rod)
+                    {
+                        FishingRod rodCopy = rod.getOne() as FishingRod;
+
+                        for (int index = 0; index < rodCopy.numAttachmentSlots.Value; index++)
+                        {
+                            rodCopy.attachments[index] = rod.attachments[index].getOne() as StardewValley.Object;
+                            rodCopy.attachments[index].Stack = rod.attachments[index].Stack;
+
+                        }
+
+                        copy = rodCopy;
+                    }
+                    else
+                    {
+                        copy = item.getOne();
+                    }
+
                     copy.Stack = item.Stack;
 
                     this.inventory.Add(copy);
